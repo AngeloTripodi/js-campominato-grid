f290da164f925c985a6ec544bd6d2112c3c2bcdd
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-30 19:25:19 +0100

    add for

--- js/script.js
+++ js/script.js
@@ -15,6 +15,8 @@ console.log(getSquare('div', 'test', mainElement));
 
 
 
+//!ADESSO DEVO GENERARE UN ADDEVENTLISTENER COLLEGATO AL PULSANTE HTML CHE COME PRIMA COSA MI FARA PARTIRE
+// ! LA FUNZIONE DI ASSEGNAZIONE DELLE CLASSI DISPLAYGRID E D-FLEX AL MAIN E POI IL FOR SOTTOSTANTE
 for (let i=1; i<=100; i++){
     const createNewSquare = getSquare('div', 'square', squareContainer, 'clicked');
     createNewSquare.classList.add('d-flex');
