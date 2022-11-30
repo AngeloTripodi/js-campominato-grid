9c32b4219c6ef017ba03803ebc3aa798118d6865
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-30 19:22:13 +0100

    addeventlistener on click

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const mainElement = document.querySelector('main');
 const squareContainer = document.querySelector('.grid');
 
-function getSquare(elm, cls, prn){
+function getSquare(elm, cls, prn, clicked){
     const newSquare = document.createElement(elm);
     newSquare.classList.add(cls);
     prn.appendChild(newSquare);
+    newSquare.addEventListener('click', function(){
+        newSquare.classList.toggle(clicked)
+    });
     return newSquare;
 }
 console.log(getSquare('div', 'test', mainElement));
@@ -13,7 +16,7 @@ console.log(getSquare('div', 'test', mainElement));
 
 
 for (let i=1; i<=100; i++){
-    const createNewSquare = getSquare('div', 'square', squareContainer);
+    const createNewSquare = getSquare('div', 'square', squareContainer, 'clicked');
     createNewSquare.classList.add('d-flex');
     createNewSquare.innerHTML =`<span class="d-flex m-auto"> ${[i]} <span>`;
 }
